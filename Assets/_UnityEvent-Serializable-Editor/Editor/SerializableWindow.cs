f07fd8c0347da713259314f61461e1d1e221d300
JeromeJ <jerome.jamart.42@gmail.com>
2017-12-19 14:03:13 +0100

    WindowEditor

---
+++ Assets/_UnityEvent-Serializable-Editor/Editor/SerializableWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+class MyWindow : EditorWindow
+{
+    // Use static so it persists when closing the window
+    // (Doesn't persist on Unity restart)
+    static public GameObject poney;
+
+    [MenuItem("Window/My Window")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+
+    void OnGUI()
+    {
+        poney = EditorGUILayout.ObjectField("Kikou", poney, typeof(GameObject), true) as GameObject;
+        // Etc
+
+        if(GUILayout.Button("Coucou") && poney != null)
+        {
+            Instantiate(poney);
+        }
+    }
+}
